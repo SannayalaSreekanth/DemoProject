00a1285139e17834a62898f96247957553d8d698
Sreekanth Sannayala <sreekanth.sannayala@atombank.co.uk>
2021-10-16 14:01:12 +0530

    login Demo

--- src/main/java/com/login/LoginFunctionality.java
+++ src/main/java/com/login/LoginFunctionality.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
  public final class LoginFunctionality {
-     private void FilePathHelper() {
+     private void FilePathHelper(String[] args) {
          String path = System.getProperty("user.dir") + "\\chromedriver.exe";
          System.setProperty("webdriver.chrome.driver", path);
          WebDriver driver = new ChromeDriver();
--- src/main/java/com/login/package-info.java
+++ src/main/java/com/login/package-info.java
@@ -1,1 +1,14 @@
 package com.login;
+/**
+ * Some javadoc. // OK
+ *
+ * @author Some javadoc. // OK
+ * @version Some javadoc. // OK
+ * @param Some javadoc. // OK
+ * @return Some javadoc. // OK
+ * @since Some javadoc. // OK
+ * @serial Some javadoc. // OK
+ * @serialField // OK
+ * @serialData // OK
+ * @deprecated Some javadoc. // OK
+ */
