39d758e6bb740e60937a3b174f59636a7a70f214
Sreekanth Sannayala <sreekanth.sannayala@atombank.co.uk>
2021-10-16 11:37:53 +0530

    commit code

---
+++ src/main/java/LoginFunctionality.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginFunctionality {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+              driver.manage().window().maximize();
+              driver.get("https://accounts.google.com/");
+//        browser.url("https://accounts.google.com/")
+//
+//                .assert.visible("input[type=email]")
+//                //.setValue({selector: 'input[type=search], abortOnFailure : false'},"sreekanth.sannayala@gmail.com")
+//                .setValue('input[type=email]',"sreekanth.sannayala@gmail.com")
+
+    }
+}
