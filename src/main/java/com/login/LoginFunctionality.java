6ba8f8586fc779f2e4853ae81aa12a7e8b7a167f
Sreekanth Sannayala <sreekanth.sannayala@atombank.co.uk>
2021-10-16 13:11:24 +0530

    login Demo

--- src/main/java/com/login/LoginFunctionality.java
+++ src/main/java/com/login/LoginFunctionality.java
@@ -1,11 +1,11 @@
+package com.login;
 //import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginFunctionality {
+public final class LoginFunctionality {
     public static void main(String[] args) {
-
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+ "\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
               driver.manage().window().maximize();
               driver.get("https://accounts.google.com/");
