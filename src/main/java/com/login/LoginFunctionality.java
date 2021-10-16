ea9f8e531cd4f6c3db8c7d854401aba9bfac5d79
Sreekanth Sannayala <sreekanth.sannayala@atombank.co.uk>
2021-10-16 14:12:43 +0530

    login Demo

--- src/main/java/com/login/LoginFunctionality.java
+++ src/main/java/com/login/LoginFunctionality.java
@@ -1,24 +1,24 @@
+/**
+ * Some javadoc. // OK
+ *
+ * @author Some javadoc. // OK
+ * @version Some javadoc. // OK
+ * @param Some javadoc. // OK
+ * @return Some javadoc. // OK
+ * @since Some javadoc. // OK
+ * @serial Some javadoc. // OK
+ * @serialField // OK
+ * @serialData // OK
+ * @deprecated Some javadoc. // OK
+ */
 package com.login;
 //import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
  public final class LoginFunctionality {
-     private void FilePathHelper(String[] args) {
-         /**
-          * Some javadoc. // OK
-          *
-          * @author Some javadoc. // OK
-          * @version Some javadoc. // OK
-          * @param Some javadoc. // OK
-          * @return Some javadoc. // OK
-          * @since Some javadoc. // OK
-          * @serial Some javadoc. // OK
-          * @serialField // OK
-          * @serialData // OK
-          * @deprecated Some javadoc. // OK
-          */
-         String path = System.getProperty("user.dir") + "\\chromedriver.exe";
+     private void FilePathHelper() {
+         final String path = System.getProperty("user.dir") + "\\chromedriver.exe";
          System.setProperty("webdriver.chrome.driver", path);
          WebDriver driver = new ChromeDriver();
          driver.manage().window().maximize();
