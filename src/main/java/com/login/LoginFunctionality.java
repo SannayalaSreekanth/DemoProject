45cdb3338d7abe31bb8be8c3050065a132a5187d
Sreekanth Sannayala <sreekanth.sannayala@atombank.co.uk>
2021-10-18 18:18:21 +0530

    login Demo

--- src/main/java/com/login/LoginFunctionality.java
+++ src/main/java/com/login/LoginFunctionality.java
@@ -1,4 +1,4 @@
-/**
+package com.login; /**
  * Some javadoc. // OK
  *
  * @author Some javadoc. // OK
@@ -11,16 +11,17 @@
  * @serialData // OK
  * @deprecated Some javadoc. // OK
  */
-package com.login;
 //import org.openqa.selenium.By;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.logging.Logger;
+import org.testng.annotations.Test;
 
+import java.util.logging.Logger;
+@Test
 //login to gmail app
  public final class LoginFunctionality {
-     public static void main(String[] args) {
+     public  void login() {
           Logger log = Logger.getLogger("InfoLogging");
          final String path = System.getProperty("user.dir") + "\\chromedriver.exe";
          System.setProperty("webdriver.chrome.driver", path);
@@ -29,13 +30,6 @@ import java.util.logging.Logger;
          driver.get("https://accounts.google.com/");
          //System.out.println("lanched the gmail");
          log.info("successfully logged into gmail");
-         driver.findElement(By.id("\"input[type=email]\""));
+        // driver.findElement(By.id("\"input[type=email]\""));
      }
-//     static void main(String[] args) {
-//        String path = System.getProperty("user.dir") + "\\chromedriver.exe";
-//        System.setProperty("webdriver.chrome.driver", path);
-//        WebDriver driver = new ChromeDriver();
-//              driver.manage().window().maximize();
-//              driver.get("https://accounts.google.com/");
-//    }
 }
