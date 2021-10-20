40ba5c0210ce87bed15ff14d69b2fde40aca70e5
Sreekanth Sannayala <sreekanth.sannayala@atombank.co.uk>
2021-10-20 17:37:21 +0530

    added two more tests

--- src/main/java/com/login/LoginFunctionality.java
+++ src/main/java/com/login/LoginFunctionality.java
@@ -23,20 +23,33 @@ import java.util.logging.Logger;
 @Test
 //login to gmail app
  public final class LoginFunctionality {
-     public  void login() {
-          Logger log = Logger.getLogger("InfoLogging");
-         //  String path = System.getProperty("user.dir") + "/src/main/resources/Drivers/Linux/chromedriver";
-         //System.out.println(System.getProperty("user.dir"));
+    public void login() {
+        Logger log = Logger.getLogger("InfoLogging");
+        //  String path = System.getProperty("user.dir") + "/src/main/resources/Drivers/Linux/chromedriver";
+        //System.out.println(System.getProperty("user.dir"));
         // System.out.println(path);
-         //System.setProperty("webdriver.chrome.driver", path);
-         WebDriverManager.chromedriver().setup();
-         ChromeOptions options = new ChromeOptions();options. addArguments("--headless");
-         WebDriver driver = new ChromeDriver(options);
-         //WebDriver driver = new ChromeDriver();
-         driver.manage().window().maximize();
-         driver.get("https://accounts.google.com/");
-         //System.out.println("lanched the gmail");
-         log.info("successfully logged into gmail");
+        //System.setProperty("webdriver.chrome.driver", path);
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        //WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://accounts.google.com/");
+        //System.out.println("lanched the gmail");
+        log.info("successfully logged into gmail");
         // driver.findElement(By.id("\"input[type=email]\""));
-     }
+    }
+
+    @Test
+    public void logReport() {
+        Logger log = Logger.getLogger("InfoLogging");
+        log.info("successfully logged into gmail");
+    }
+
+    @Test
+    public void logout() {
+        System.out.println("abcd");
+    }
+
 }
